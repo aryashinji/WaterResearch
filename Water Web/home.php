1cc0574e4f21ae81a4da6c2015855488d52862b8
aryashinji <ashura.shinji@gmail.com>
2015-11-05 15:54:27 +0800

    update search

--- Water Web/home.php
+++ Water Web/home.php
@@ -17,7 +17,6 @@ if(isset($_POST['alamat']) || isset($_POST['latitude']) || isset($_POST['longitu
 
 <head>    
   <meta charset="utf-8">
-  <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">    
   <meta name="viewport" content="width=device-width, initial-scale=1">    
   <meta name="description" content="">    
@@ -37,7 +36,8 @@ if(isset($_POST['alamat']) || isset($_POST['latitude']) || isset($_POST['longitu
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>        
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>    
   <![endif]-->
-  <style>
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <style>
       html, body {
         height: 100%;
         margin: 0;
@@ -46,49 +46,50 @@ if(isset($_POST['alamat']) || isset($_POST['latitude']) || isset($_POST['longitu
       #map {
         height: 100%;
       }
-      .controls {
-        margin-top: 10px;
-        border: 1px solid transparent;
-        border-radius: 2px 0 0 2px;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        height: 32px;
-        outline: none;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-      }
+.controls {
+  margin-top: 10px;
+  border: 1px solid transparent;
+  border-radius: 2px 0 0 2px;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  height: 32px;
+  outline: none;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+}
 
-      #pac-input {
-        background-color: #fff;
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-        margin-left: 12px;
-        padding: 0 11px 0 13px;
-        text-overflow: ellipsis;
-        width: 300px;
-      }
+#pac-input {
+  background-color: #fff;
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: 300;
+  margin-left: 12px;
+  padding: 0 11px 0 13px;
+  text-overflow: ellipsis;
+  width: 300px;
+}
 
-      #pac-input:focus {
-        border-color: #4d90fe;
-      }
+#pac-input:focus {
+  border-color: #4d90fe;
+}
 
-      .pac-container {
-        font-family: Roboto;
-      }
+.pac-container {
+  font-family: Roboto;
+}
 
-      #type-selector {
-        color: #fff;
-        background-color: #4d90fe;
-        padding: 5px 11px 0px 11px;
-      }
+#type-selector {
+  color: #fff;
+  background-color: #4d90fe;
+  padding: 5px 11px 0px 11px;
+}
 
-      #type-selector label {
-        font-family: Roboto;
-        font-size: 13px;
-        font-weight: 300;
-      } 
+#type-selector label {
+  font-family: Roboto;
+  font-size: 13px;
+  font-weight: 300;
+}
 
     </style>
+    <title>Places Searchbox</title>
     <style>
       #target {
         width: 345px;
@@ -238,77 +239,78 @@ if(isset($_POST['alamat']) || isset($_POST['latitude']) || isset($_POST['longitu
                          </script>
                          </head>
                          <body onload="load()">
+
                             <input id="pac-input" class="controls" type="text" placeholder="Search Box">
-                            <div  id="map" style="width: 100%;  height: 90%"></div>
+                            <div id="map" style="width: 100%;  height: 90%"></div>
                             <script>
-                              // This example adds a search box to a map, using the Google Place Autocomplete
-                              // feature. People can enter geographical searches. The search box will return a
-                              // pick list containing a mix of places and predicted search terms.
+                        // This example adds a search box to a map, using the Google Place Autocomplete
+                        // feature. People can enter geographical searches. The search box will return a
+                        // pick list containing a mix of places and predicted search terms.
 
-                              function initAutocomplete() {
-                                var map = new google.maps.Map(document.getElementById('map'), {
-                                  center: {lat: -33.8688, lng: 151.2195},
-                                  zoom: 13,
-                                  mapTypeId: google.maps.MapTypeId.ROADMAP
-                                });
+                        function initAutocomplete() {
+                          var map = new google.maps.Map(document.getElementById('map'), {
+                            center: {lat: -33.8688, lng: 151.2195},
+                            zoom: 13,
+                            mapTypeId: google.maps.MapTypeId.ROADMAP
+                          });
 
-                                // Create the search box and link it to the UI element.
-                                var input = document.getElementById('pac-input');
-                                var searchBox = new google.maps.places.SearchBox(input);
-                                map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+                          // Create the search box and link it to the UI element.
+                          var input = document.getElementById('pac-input');
+                          var searchBox = new google.maps.places.SearchBox(input);
+                          map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-                                // Bias the SearchBox results towards current map's viewport.
-                                map.addListener('bounds_changed', function() {
-                                  searchBox.setBounds(map.getBounds());
-                                });
+                          // Bias the SearchBox results towards current map's viewport.
+                          map.addListener('bounds_changed', function() {
+                            searchBox.setBounds(map.getBounds());
+                          });
 
-                                var markers = [];
-                                // [START region_getplaces]
-                                // Listen for the event fired when the user selects a prediction and retrieve
-                                // more details for that place.
-                                searchBox.addListener('places_changed', function() {
-                                  var places = searchBox.getPlaces();
+                          var markers = [];
+                          // [START region_getplaces]
+                          // Listen for the event fired when the user selects a prediction and retrieve
+                          // more details for that place.
+                          searchBox.addListener('places_changed', function() {
+                            var places = searchBox.getPlaces();
 
-                                  if (places.length == 0) {
-                                    return;
-                                  }
+                            if (places.length == 0) {
+                              return;
+                            }
 
-                                  // Clear out the old markers.
-                                  markers.forEach(function(marker) {
-                                    marker.setMap(null);
-                                  });
-                                  markers = [];
+                            // Clear out the old markers.
+                            markers.forEach(function(marker) {
+                              marker.setMap(null);
+                            });
+                            markers = [];
 
-                                  // For each place, get the icon, name and location.
-                                  var bounds = new google.maps.LatLngBounds();
-                                  places.forEach(function(place) {
-                                    var icon = {
-                                      url: place.icon,
-                                      size: new google.maps.Size(71, 71),
-                                      origin: new google.maps.Point(0, 0),
-                                      anchor: new google.maps.Point(17, 34),
-                                      scaledSize: new google.maps.Size(25, 25)
-                                    };
+                            // For each place, get the icon, name and location.
+                            var bounds = new google.maps.LatLngBounds();
+                            places.forEach(function(place) {
+                              var icon = {
+                                url: place.icon,
+                                size: new google.maps.Size(71, 71),
+                                origin: new google.maps.Point(0, 0),
+                                anchor: new google.maps.Point(17, 34),
+                                scaledSize: new google.maps.Size(25, 25)
+                              };
 
-                                    // Create a marker for each place.
-                                    markers.push(new google.maps.Marker({
-                                      map: map,
-                                      icon: icon,
-                                      title: place.name,
-                                      position: place.geometry.location
-                                    }));
+                              // Create a marker for each place.
+                              markers.push(new google.maps.Marker({
+                                map: map,
+                                icon: icon,
+                                title: place.name,
+                                position: place.geometry.location
+                              }));
 
-                                    if (place.geometry.viewport) {
-                                      // Only geocodes have viewport.
-                                      bounds.union(place.geometry.viewport);
-                                    } else {
-                                      bounds.extend(place.geometry.location);
-                                    }
-                                  });
-                                  map.fitBounds(bounds);
-                                });
-                                // [END region_getplaces]
+                              if (place.geometry.viewport) {
+                                // Only geocodes have viewport.
+                                bounds.union(place.geometry.viewport);
+                              } else {
+                                bounds.extend(place.geometry.location);
                               }
+                            });
+                            map.fitBounds(bounds);
+                          });
+                          // [END region_getplaces]
+                        }
 
 
                             </script>
@@ -327,6 +329,8 @@ if(isset($_POST['alamat']) || isset($_POST['latitude']) || isset($_POST['longitu
     </div>
     <!-- /#wrapper -->
     <!-- jQuery -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAL0QJzVzAfGHqk135byL6SCS_-Kv8TLwk&libraries=places&callback=initAutocomplete"
+                                 async defer></script>
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
