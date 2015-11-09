1aec0cb7d96ba0847a74d2eee5ce49ea6ab33091
aryashinji <ashura.shinji@gmail.com>
2015-11-09 13:29:54 +0800

    Fix search feature

--- waterbaru/home.php
+++ waterbaru/home.php
@@ -191,70 +191,7 @@ if(isset($_POST['alamat']) || isset($_POST['latitude']) || isset($_POST['longitu
                              });
                              var infoWindow = new google.maps.InfoWindow;
 
-                             // Rubah nama ini sesuai sengan script php yg menghasilkan script XML
-                             downloadUrl("test.php", function(data) {
-                               var xml = data.responseXML;
-                               var markers = xml.documentElement.getElementsByTagName("marker");
-                               for (var i = 0; i < markers.length; i++) {
-                                 var name = markers[i].getAttribute("name");
-                                 var address = markers[i].getAttribute("address");
-                                 var type = markers[i].getAttribute("type");
-                                 var point = new google.maps.LatLng(
-                                     parseFloat(markers[i].getAttribute("lat")),
-                                    parseFloat(markers[i].getAttribute("lng")));
-                                 var html = "<b>" + name + "</b> <br/>" + address;
-                                 var icon = customIcons[type] || {};
-                                 var marker = new google.maps.Marker({
-                                   map: map,
-                                   position: point,
-                                   icon: icon.icon,
-                                   shadow: icon.shadow
-                                 });
-                                 bindInfoWindow(marker, map, infoWindow, html);
-
-                               }
-                             });
-                           }
-                           function bindInfoWindow(marker, map, infoWindow, html) {
-                             google.maps.event.addListener(marker, 'click', function() {
-                               infoWindow.setContent(html);
-                               infoWindow.open(map, marker);
-                             });
-                           }
-                           function downloadUrl(url, callback) {
-                             var request = window.ActiveXObject ?
-                                 new ActiveXObject('Microsoft.XMLHTTP') :
-                                 new XMLHttpRequest;
-                             request.onreadystatechange = function() {
-                               if (request.readyState == 4) {
-                                 request.onreadystatechange = doNothing;
-                                 callback(request, request.status);
-                               }
-                             };
-                             request.open('GET', url, true);
-                             request.send(null);
-                           }
-                           function doNothing() {}
-                           //]]>
-                         </script>
-                         </head>
-                         <body onload="load()">
-
-                            <input id="pac-input" class="controls" type="text" placeholder="Search Box">
-                            <div id="map" style="width: 100%;  height: 90%"></div>
-                            <script>
-                        // This example adds a search box to a map, using the Google Place Autocomplete
-                        // feature. People can enter geographical searches. The search box will return a
-                        // pick list containing a mix of places and predicted search terms.
-
-                        function initAutocomplete() {
-                          var map = new google.maps.Map(document.getElementById('map'), {
-                            center: {lat: -33.8688, lng: 151.2195},
-                            zoom: 13,
-                            mapTypeId: google.maps.MapTypeId.ROADMAP
-                          });
-
-                          // Create the search box and link it to the UI element.
+                             // Create the search box and link it to the UI element.
                           var input = document.getElementById('pac-input');
                           var searchBox = new google.maps.places.SearchBox(input);
                           map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
@@ -309,11 +246,58 @@ if(isset($_POST['alamat']) || isset($_POST['latitude']) || isset($_POST['longitu
                             });
                             map.fitBounds(bounds);
                           });
-                          // [END region_getplaces]
-                        }
 
+                             // Rubah nama ini sesuai sengan script php yg menghasilkan script XML
+                             downloadUrl("test.php", function(data) {
+                               var xml = data.responseXML;
+                               var markers = xml.documentElement.getElementsByTagName("marker");
+                               for (var i = 0; i < markers.length; i++) {
+                                 var name = markers[i].getAttribute("name");
+                                 var address = markers[i].getAttribute("address");
+                                 var type = markers[i].getAttribute("type");
+                                 var point = new google.maps.LatLng(
+                                     parseFloat(markers[i].getAttribute("lat")),
+                                    parseFloat(markers[i].getAttribute("lng")));
+                                 var html = "<b>" + name + "</b> <br/>" + address;
+                                 var icon = customIcons[type] || {};
+                                 var marker = new google.maps.Marker({
+                                   map: map,
+                                   position: point,
+                                   icon: icon.icon,
+                                   shadow: icon.shadow
+                                 });
+                                 bindInfoWindow(marker, map, infoWindow, html);
 
-                            </script>
+                               }
+                             });
+                           }
+                           function bindInfoWindow(marker, map, infoWindow, html) {
+                             google.maps.event.addListener(marker, 'click', function() {
+                               infoWindow.setContent(html);
+                               infoWindow.open(map, marker);
+                             });
+                           }
+                           function downloadUrl(url, callback) {
+                             var request = window.ActiveXObject ?
+                                 new ActiveXObject('Microsoft.XMLHTTP') :
+                                 new XMLHttpRequest;
+                             request.onreadystatechange = function() {
+                               if (request.readyState == 4) {
+                                 request.onreadystatechange = doNothing;
+                                 callback(request, request.status);
+                               }
+                             };
+                             request.open('GET', url, true);
+                             request.send(null);
+                           }
+                           function doNothing() {}
+                           //]]>
+                         </script>
+                         </head>
+                         <body onload="load()">
+
+                            <input id="pac-input" class="controls" type="text" placeholder="Search Box">
+                            <div id="map" style="width: 100%;  height: 90%"></div>
                          </body>
                     </div>
                 </div>
